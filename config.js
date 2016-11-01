04b913b34e764a31912d9fc5eb27553616d58c95
Octavio Turra <octavio.turra@gmail.com>
2016-11-01 20:07:18 -0200

    Server Key

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.GoogleSamples = window.GoogleSamples || {};
 window.GoogleSamples.Config = window.GoogleSamples.Config || {
-  gcmAPIKey: 'AIzaSyDYSHlHyKFY7XlIOEIXxMqUDH4bVSaIQmo'
+  gcmAPIKey: 'AIzaSyB2EcubXJrvNv3ZWIkWSNSQ22gz4t9mp9Q'
 };
